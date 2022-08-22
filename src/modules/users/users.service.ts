a11b2f7bacb49d7e2eb4bffeee8e8f068d375f36
Juan Camayo <jd.camayo@gmail.com>
2022-08-22 13:41:04 -0500

    Create - Users module

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,136 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/libs/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async hashPassword(password: string) {
+    return await bcrypt.hash(password, 10);
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    if (await this.findOneByEmail(createUserDto.email)) {
+      throw new BadRequestException('Email is already registered');
+    }
+    const hashedPassword = await this.hashPassword(createUserDto.password);
+    const user = await this.prisma.user.create({
+      data: {
+        email: createUserDto.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+        createdAt: true,
+        updatedAt: true,
+        email: true,
+        role: true,
+      },
+    });
+    return user;
+  }
+
+  async findAll(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.UserWhereUniqueInput;
+    where?: Prisma.UserWhereInput;
+    orderBy?: Prisma.UserOrderByWithRelationInput;
+  }) {
+    const { skip, take, cursor, where, orderBy } = params;
+    const users = await this.prisma.user.findMany({
+      select: {
+        id: true,
+        createdAt: true,
+        updatedAt: true,
+        email: true,
+        role: true,
+      },
+      skip,
+      take,
+      cursor,
+      where: where
+        ? where
+        : {
+            blocked: false,
+          },
+      orderBy,
+    });
+    return users;
+  }
+
+  async findOne(id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        id: true,
+        createdAt: true,
+        updatedAt: true,
+        email: true,
+        role: true,
+      },
+    });
+    return user;
+  }
+
+  async findOneByEmail(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+      select: {
+        id: true,
+        createdAt: true,
+        updatedAt: true,
+        email: true,
+        role: true,
+      },
+    });
+    return user;
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    if (updateUserDto.password) {
+      updateUserDto.password = await this.hashPassword(updateUserDto.password);
+    }
+    const user = await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: updateUserDto,
+      select: {
+        id: true,
+        createdAt: true,
+        updatedAt: true,
+        email: true,
+        role: true,
+      },
+    });
+    return user;
+  }
+
+  async remove(id: string) {
+    const user = await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        blocked: true,
+      },
+      select: {
+        id: true,
+        createdAt: true,
+        updatedAt: true,
+        email: true,
+        role: true,
+      },
+    });
+    return user;
+  }
+}
